2b2b15ccfc19d35fbd1e7098a49711d27fdeaed2
Jigar Patel <jigar.patel@servian.com>
2021-03-12 16:50:58 +1300

    Add more configuration options for connecting with a k8s cluster As discussed with Prudential, this is similar to the configuration options for the vault kubernetes auth plugin here: https://www.vaultproject.io/docs/auth/kubernetes

--- local_test.sh
+++ local_test.sh
@@ -4,6 +4,8 @@ export VAULT_ADDR="http://127.0.0.1:8200" \
 && vault secrets list \
 && vault write k8s/config max_ttl=50 token_reviewer_jwt="some jwt token" \
 kubernetes_host="http://localhost:5555" kubernetes_ca_cert="some cert data" \
+pem_keys="key1,key2" issuer="some issuer" \
+disable_iss_validation="true" disable_local_ca_jwt="true" \
 allowed_roles="r1,r2" allowed_cluster_roles="r1c,r2c" \
 && vault read k8s/config \
 && vault read k8s/service_account namespace="my-awesome-ns" role_name="r1"
--- pkg/config_k8s_service_account.go
+++ pkg/config_k8s_service_account.go
@@ -12,18 +12,26 @@ const keyMaxTTL = "max_ttl"
 const keyTokenReviewerJWT = "token_reviewer_jwt"
 const keyKubernetesHost = "kubernetes_host"
 const keyKubernetesCACert = "kubernetes_ca_cert"
+const keyPemKeys = "pem_keys"
+const keyIssuer = "issuer"
+const keyDisableISSValidation = "disable_iss_validation"
+const keyDisableLocalCaJWT = "disable_local_ca_jwt"
 const keyAllowedRoles = "allowed_roles"
 const keyAllowedClusterRoles = "allowed_cluster_roles"
 
 const configPath = "config"
 
 type PluginConfig struct {
-	MaxTTL              int      `json:"max_ttl"`
-	TokenReviewerJWT    string   `json:"token_reviewer_jwt"`
-	KubernetesHost      string   `json:"kubernetes_host"`
-	KubernetesCACert    string   `json:"kubernetes_ca_cert"`
-	AllowedRoles        []string `json:"allowed_roles"`
-	AllowedClusterRoles []string `json:"allowed_cluster_roles"`
+	MaxTTL               int      `json:"max_ttl"`
+	TokenReviewerJWT     string   `json:"token_reviewer_jwt"`
+	KubernetesHost       string   `json:"kubernetes_host"`
+	KubernetesCaCert     string   `json:"kubernetes_ca_cert"`
+	PemKeys              []string `json:"pem_keys"`
+	Issuer               string   `json:"issuer"`
+	DisableISSValidation bool     `json:"disable_iss_validation"`
+	DisableLocalCaJWT    bool     `json:"disable_local_ca_jwt"`
+	AllowedRoles         []string `json:"allowed_roles"`
+	AllowedClusterRoles  []string `json:"allowed_cluster_roles"`
 }
 
 func configK8sServiceAccount(b *backend) *framework.Path {
@@ -39,6 +47,9 @@ func configK8sServiceAccount(b *backend) *framework.Path {
 				Description: `A service account JWT used to access the
 TokenReview API to validate other JWTs during login. If not set
 the JWT used for login will be used to access the API.`,
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "Token Reviewer JWT",
+				},
 			},
 			keyKubernetesHost: {
 				Type:        framework.TypeString,
@@ -47,13 +58,49 @@ the JWT used for login will be used to access the API.`,
 			keyKubernetesCACert: {
 				Type:        framework.TypeString,
 				Description: "PEM encoded CA cert for use by the TLS client used to talk with the API.",
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "Kubernetes CA Certificate",
+				},
+			},
+			keyPemKeys: {
+				Type: framework.TypeCommaStringSlice,
+				Description: `Optional list of PEM-formated public keys or certificates
+used to verify the signatures of kubernetes service account
+JWTs. If a certificate is given, its public key will be
+extracted. Not every installation of Kuberentes exposes these keys.`,
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "Service account verification keys",
+				},
+			},
+			keyIssuer: {
+				Type:        framework.TypeString,
+				Description: "Optional JWT issuer. If no issuer is specified, then this plugin will use kubernetes.io/serviceaccount as the default issuer.",
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "JWT Issuer",
+				},
+			},
+			keyDisableISSValidation: {
+				Type:        framework.TypeBool,
+				Description: "Disable JWT issuer validation. Allows to skip ISS validation.",
+				Default:     false,
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "Disable JWT Issuer Validation",
+				},
+			},
+			keyDisableLocalCaJWT: {
+				Type:        framework.TypeBool,
+				Description: "Disable defaulting to the local CA cert and service account JWT when running in a Kubernetes pod",
+				Default:     false,
+				DisplayAttrs: &framework.DisplayAttributes{
+					Name: "Disable use of local CA and service account JWT",
+				},
 			},
 			keyAllowedRoles: {
-				Type:        framework.TypeStringSlice,
+				Type:        framework.TypeCommaStringSlice,
 				Description: "Kubernetes roles that can be assigned to service accounts created by this plugin.",
 			},
 			keyAllowedClusterRoles: {
-				Type:        framework.TypeStringSlice,
+				Type:        framework.TypeCommaStringSlice,
 				Description: "Kubernetes cluster roles that can be assigned to service accounts created by this plugin.",
 			},
 		},
@@ -79,16 +126,24 @@ func (b *backend) handleConfigWrite(ctx context.Context, req *logical.Request, d
 	tokenReviewerJWT := d.Get(keyTokenReviewerJWT).(string)
 	kubernetesHost := d.Get(keyKubernetesHost).(string)
 	kubernetesCACert := d.Get(keyKubernetesCACert).(string)
+	pemKeys := d.Get(keyPemKeys).([]string)
+	issuer := d.Get(keyIssuer).(string)
+	disableISSValidation := d.Get(keyDisableISSValidation).(bool)
+	disableLocalCaJWT := d.Get(keyDisableLocalCaJWT).(bool)
 	allowedRoles := d.Get(keyAllowedRoles).([]string)
 	allowedClusterRoles := d.Get(keyAllowedClusterRoles).([]string)
 	b.Logger().Info(fmt.Sprintf("TTL specified is: %d", ttl))
 	config := PluginConfig{
-		MaxTTL:              ttl,
-		TokenReviewerJWT:    tokenReviewerJWT,
-		KubernetesHost:      kubernetesHost,
-		KubernetesCACert:    kubernetesCACert,
-		AllowedRoles:        allowedRoles,
-		AllowedClusterRoles: allowedClusterRoles,
+		MaxTTL:               ttl,
+		TokenReviewerJWT:     tokenReviewerJWT,
+		KubernetesHost:       kubernetesHost,
+		KubernetesCaCert:     kubernetesCACert,
+		PemKeys:              pemKeys,
+		Issuer:               issuer,
+		DisableISSValidation: disableISSValidation,
+		DisableLocalCaJWT:    disableLocalCaJWT,
+		AllowedRoles:         allowedRoles,
+		AllowedClusterRoles:  allowedClusterRoles,
 	}
 	entry, err := logical.StorageEntryJSON(configPath, config)
 	if err != nil {
@@ -108,12 +163,16 @@ func (b *backend) handleConfigRead(ctx context.Context, req *logical.Request, d 
 	} else {
 		resp := &logical.Response{
 			Data: map[string]interface{}{
-				keyMaxTTL:              config.MaxTTL,
-				keyTokenReviewerJWT:    config.TokenReviewerJWT,
-				keyKubernetesHost:      config.KubernetesHost,
-				keyKubernetesCACert:    config.KubernetesCACert,
-				keyAllowedRoles:        config.AllowedRoles,
-				keyAllowedClusterRoles: config.AllowedClusterRoles,
+				keyMaxTTL:               config.MaxTTL,
+				keyTokenReviewerJWT:     config.TokenReviewerJWT,
+				keyKubernetesHost:       config.KubernetesHost,
+				keyKubernetesCACert:     config.KubernetesCaCert,
+				keyPemKeys:              config.PemKeys,
+				keyIssuer:               config.Issuer,
+				keyDisableLocalCaJWT:    config.DisableLocalCaJWT,
+				keyDisableISSValidation: config.DisableISSValidation,
+				keyAllowedRoles:         config.AllowedRoles,
+				keyAllowedClusterRoles:  config.AllowedClusterRoles,
 			},
 		}
 		return resp, nil
